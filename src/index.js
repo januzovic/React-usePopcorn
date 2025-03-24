d1490d79d6911e7f34d36f16c0405eaa4a629291
Januzovic <sale.business95@gmail.com>
2025-03-24 14:51:59 +0100

    Work in progres

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StaticRating maxRating={5} />
+    <StaticRating
+      maxRating={5}
+      messages={('Terrible', 'Bad', 'Okay', 'Good', 'Amazing')}
+    />
+    <StaticRating size={24} color='red' className='test' />
   </React.StrictMode>
 );
